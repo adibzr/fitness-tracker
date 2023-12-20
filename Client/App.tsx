61edd9a2a7d345f887df32673683755a986555d6
Adib Raed <adibraed1@gmail.com>
2023-12-20 15:42:32 +0000

    added boiler plate

---
+++ Client/App.tsx
@@ -0,0 +1,8 @@
+
+import './App.css'
+
+function App() {
+  return <div>test</div>
+}
+
+export default App
---
+++ Client/store/store.ts
@@ -0,0 +1,22 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+
+const store = configureStore({
+  //Add reduces here
+  reducer: {
+    
+  },
+});
+
+export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
